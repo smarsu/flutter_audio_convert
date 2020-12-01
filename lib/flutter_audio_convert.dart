01894d37b12c5b91873165dac56f7e6eb380114d
smarsufan <smarsufan@tencent.com>
2020-12-01 16:16:08 +0800

    Add Isolate

--- lib/flutter_audio_convert.dart
+++ lib/flutter_audio_convert.dart
@@ -31,18 +31,30 @@ final int Function(int, int, int, int) _audioConvertToThumbnail =
         .lookup<NativeFunction<Int32 Function(Int64, Int64, Int64, Int32)>>("to_thumbnail")
         .asFunction();
 
+final int Function(int, int, double, double) _audioConvertToCut =
+    _audioConvertLib
+        .lookup<NativeFunction<Int32 Function(Int64, Int64, Double, Double)>>("to_m4a_cut")
+        .asFunction();
+
 int _lastErrRet = 0;
+
+Map<String, dynamic> cache = {};
 
 int getLastErrRet() {
   return _lastErrRet;
 }
 
 Future<String> appDocPath(path) async {
-  var dir = (await getApplicationDocumentsDirectory()).path;
+  var dir = (await getTemporaryDirectory()).path;
   return '$dir/$path';
 }
 
 Future<String> toM4A(String input, {String output, String extend='mp4'}) async {
+  String key = 'toM4A_${input}_${extend}';
+  if (cache.containsKey(key)) {
+    return cache[key];
+  }
+
   if (output == null) {
     output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}.$extend');
   }
@@ -55,20 +67,29 @@ Future<String> toM4A(String input, {String output, String extend='mp4'}) async {
   _lastErrRet = convertRet;
   if (convertRet < 0) {
     // Failed
+    cache[key] = input;
     return input;
   }
   else if (convertRet == 0) {
     // Success
+    cache[key] = output;
     return output;
   }
   else if (convertRet == 1) {
     // The input is M4A format.
+    cache[key] = input;
     return input;
   }
+  cache[key] = output;
   return output;
 }
 
 Future<String> toVolume(String input, {String output, double volume=-10, String extend='mp4'}) async {
+  String key = 'toVolume_${input}_${volume}_${extend}';
+  if (cache.containsKey(key)) {
+    return cache[key];
+  }
+
   if (output == null) {
     output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}.$extend');
   }
@@ -81,16 +102,24 @@ Future<String> toVolume(String input, {String output, double volume=-10, String 
   _lastErrRet = convertRet;
   if (convertRet < 0) {
     // Failed
+    cache[key] = input;
     return input;
   }
   else if (convertRet == 0) {
     // Success
+    cache[key] = output;
     return output;
   }
+  cache[key] = output;
   return output;
 }
 
 Future<int> toDuration(String input) async {
+  String key = 'toDuration_${input}';
+  if (cache.containsKey(key)) {
+    return cache[key];
+  }
+
   Int8P inputPtr = Int8P.fromString(input);
   int duration = _audioConvertToDuration(inputPtr.address);
   inputPtr.dispose();
@@ -98,26 +127,46 @@ Future<int> toDuration(String input) async {
   _lastErrRet = duration;
   if (duration < 0) {
     // Failed
+    cache[key] = 0;
     return 0;
   }
   else {
+    cache[key] = duration;
     return duration;
   }
 }
 
 Future<List<String>> toThumbnail(String input, List<double> timesInMs, {List<String> outputs}) async {
+  String key = 'toThumbnail_${input}_${timesInMs}';
+  if (cache.containsKey(key)) {
+    return cache[key];
+  }
+
   List<Int8P> outputPtrs = [];
   List<int> outputAddress = [];
+  // if (outputs == null) {
+  //   outputs = [];
+  //   for (var time in timesInMs) {
+  //     String output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}_$time.jpg');
+  //     outputs.add(output);
+
+  //     Int8P outputPtr = Int8P.fromString(output);
+  //     outputPtrs.add(outputPtr);
+  //     outputAddress.add(outputPtr.address);
+  //   }
+  // }
   if (outputs == null) {
     outputs = [];
     for (var time in timesInMs) {
       String output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}_$time.jpg');
       outputs.add(output);
-
-      Int8P outputPtr = Int8P.fromString(output);
-      outputPtrs.add(outputPtr);
-      outputAddress.add(outputPtr.address);
     }
+  }
+  for (int idx = 0; idx < outputs.length; ++idx) {
+    String output = outputs[idx];
+    Int8P outputPtr = Int8P.fromString(output);
+    outputPtrs.add(outputPtr);
+    outputAddress.add(outputPtr.address);
   }
   Int8P inputPtr = Int8P.fromString(input);
   Int64P outputAddressPtr = Int64P.fromList(outputAddress);
@@ -134,5 +183,41 @@ Future<List<String>> toThumbnail(String input, List<double> timesInMs, {List<Str
 
   _lastErrRet = convertRet;
 
+  cache[key] = outputs;
   return outputs;
+}
+
+Future<String> toCut(String input, double startMs, double endMs, {String output, String extend='mp4'}) async {
+  String key = 'toCut_${input}_${extend}';
+  if (cache.containsKey(key)) {
+    return cache[key];
+  }
+
+  if (output == null) {
+    output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}.$extend');
+  }
+  Int8P inputPtr = Int8P.fromString(input);
+  Int8P outputPtr = Int8P.fromString(output);
+  int convertRet = _audioConvertToCut(inputPtr.address, outputPtr.address, startMs, endMs);
+  inputPtr.dispose();
+  outputPtr.dispose();
+  print('In toCut, convertRet ... $convertRet');
+  _lastErrRet = convertRet;
+  if (convertRet < 0) {
+    // Failed
+    cache[key] = input;
+    return input;
+  }
+  else if (convertRet == 0) {
+    // Success
+    cache[key] = output;
+    return output;
+  }
+  else if (convertRet == 1) {
+    // The input is M4A format.
+    cache[key] = input;
+    return input;
+  }
+  cache[key] = output;
+  return output;
 }
---
+++ lib/isolate.dart
@@ -0,0 +1,164 @@
+import 'dart:isolate';
+
+import 'package:flutter_audio_convert/flutter_audio_convert.dart';
+
+class AudioConvert {
+  /* Public */
+
+  static Future<String> toM4AAsync(String input, {String output, String extend='mp4'}) async {
+    await _initialized;
+
+    if (output == null) {
+      output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}.$extend');
+    }
+
+    ReceivePort receivePort = ReceivePort();
+    _toM4ASendPort.send([receivePort.sendPort, input, output, extend]);
+    return await receivePort.first;
+  }
+
+  static Future<String> toVolumeAsync(String input, {String output, double volume=-10, String extend='mp4'}) async {
+    await _initialized;
+
+    if (output == null) {
+      output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}.$extend');
+    }
+
+    ReceivePort receivePort = ReceivePort();
+    _toVolumeSendPort.send([receivePort.sendPort, input, output, volume, extend]);
+    return await receivePort.first;
+  }
+
+  static Future<List<String>> toThumbnailAsync(String input, List<double> timesInMs, {List<String> outputs}) async {
+    await _initialized;
+
+    if (outputs == null) {
+      outputs = [];
+      for (var time in timesInMs) {
+        String output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}_$time.jpg');
+        outputs.add(output);
+      }
+    }
+
+    ReceivePort receivePort = ReceivePort();
+    _toThumbnailSendPort.send([receivePort.sendPort, input, timesInMs, outputs]);
+    return await receivePort.first;
+  }
+
+  static Future<String> toCutAsync(String input, double startMs, double endMs, {String output, String extend='mp4'}) async {
+    await _initialized;
+
+    if (output == null) {
+      output = await appDocPath('${DateTime.now().microsecondsSinceEpoch}.$extend');
+    }
+
+    ReceivePort receivePort = ReceivePort();
+    _toCutSendPort.send([receivePort.sendPort, input, startMs, endMs, output, extend]);
+    return await receivePort.first;
+  }
+
+  /* Private */
+
+  static Future<void> _initialized = _init();
+
+  static SendPort _toM4ASendPort;
+  static SendPort _toCutSendPort;
+  static SendPort _toVolumeSendPort;
+  static SendPort _toThumbnailSendPort;
+  static Future<void> _init() async {
+    // 1. Init ToM4A
+    {
+      final toM4AReceive = ReceivePort();
+      await Isolate.spawn(_toM4AEntryFunction, toM4AReceive.sendPort);
+      _toM4ASendPort = await toM4AReceive.first;
+    }
+
+    // 2. Init ToCut
+    {
+      final toCutReceive = ReceivePort();
+      await Isolate.spawn(_toCutEntryFunction, toCutReceive.sendPort);
+      _toCutSendPort = await toCutReceive.first;
+    }
+
+    // 3. Init ToVolume
+    {
+      final toVolumeReceive = ReceivePort();
+      await Isolate.spawn(_toThumbnailEntryFunction, toVolumeReceive.sendPort);
+      _toThumbnailSendPort = await toVolumeReceive.first;
+    }
+
+    // 4. Init Thumbnail
+    {
+      final toThumbnailReceive = ReceivePort();
+      await Isolate.spawn(_toVolumeEntryFunction, toThumbnailReceive.sendPort);
+      _toVolumeSendPort = await toThumbnailReceive.first;
+    }
+
+    print('AudioConvert Init Success!');
+  }
+
+  static void _toM4AEntryFunction(SendPort sendPort) async {
+    ReceivePort receivePort = ReceivePort();
+    sendPort.send(receivePort.sendPort);
+
+    await for (var msg in receivePort) {
+      SendPort callbackPort = msg[0];
+      String input = msg[1];
+      String output = msg[2];
+      String extend = msg[3];
+
+      String result = await toM4A(input, output: output, extend: extend);
+      callbackPort.send(result);
+    }
+  }
+
+  static void _toVolumeEntryFunction(SendPort sendPort) async {
+    ReceivePort receivePort = ReceivePort();
+    sendPort.send(receivePort.sendPort);
+
+    await for (var msg in receivePort) {
+      SendPort callbackPort = msg[0];
+      String input = msg[1];
+      String output = msg[2];
+      double volume = msg[3];
+      String extend = msg[4];
+
+      String result = await toVolume(input, output: output, volume: volume, extend: extend);
+      callbackPort.send(result);
+    }
+  }
+
+
+
+  static void _toCutEntryFunction(SendPort sendPort) async {
+    ReceivePort receivePort = ReceivePort();
+    sendPort.send(receivePort.sendPort);
+
+    await for (var msg in receivePort) {
+      SendPort callbackPort = msg[0];
+      String input = msg[1];
+      double startMs = msg[2];
+      double endMs = msg[3];
+      String output = msg[4];
+      String extend = msg[5];
+
+      String result = await toCut(input, startMs, endMs, output: output, extend: extend);
+      callbackPort.send(result);
+    }
+  }
+
+  static void _toThumbnailEntryFunction(SendPort sendPort) async {
+    ReceivePort receivePort = ReceivePort();
+    sendPort.send(receivePort.sendPort);
+
+    await for (var msg in receivePort) {
+      SendPort callbackPort = msg[0];
+      String input = msg[1];
+      List<double> timesInMs = msg[2];
+      List<String> outputs = msg[3];
+
+      List<String> result = await toThumbnail(input, timesInMs, outputs: outputs);
+      callbackPort.send(result);
+    }
+  }
+}
